7d2d540c7e8ae8715e46aa210e5ae30a0589e67d
ffakhira <fakhiraalysa03@gmail.com>
2024-05-02 02:20:37 +0800

    save

--- database/migrations/2024_05_01_163256_create_p_b__publications_table.php
+++ database/migrations/2024_05_01_163256_create_p_b__publications_table.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('p_b__publications', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string('PB_ID')->primary();
+            $table->string('PB_Type');
+            $table->string('PB_Title');
+            $table->string('PB_Author');
+            $table->string('PB_Uni');
+            $table->string('PB_Course');
+            $table->string('PB_Keyword');
+            $table->date('PB_Date');
+            $table->foreign('ED_ID')->references('ED_ID')->on(ED_ExpertDomain)->onDelete('cascade');
+            $table->foreign('P_platinumID')->references('P_platinumID')->on(P_platinum)->onDelete('cascade');;
+            $table->foreign('PR_ID')->references('PR_ID')->on(PR_PublicationReport)->onDelete('cascade');;
         });
     }
 
