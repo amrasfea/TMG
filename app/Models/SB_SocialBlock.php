1ab57a573fbc3a56db555afa07beb6c36606f7ed
ffakhira <fakhiraalysa03@gmail.com>
2024-05-02 00:03:43 +0800

    table

---
+++ app/Models/SB_SocialBlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SB_SocialBlock extends Model
+{
+    use HasFactory;
+}
