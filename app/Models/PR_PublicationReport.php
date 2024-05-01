ad2d6754a1b661c943f8df4289b893a06ab6ac4a
ffakhira <fakhiraalysa03@gmail.com>
2024-05-02 00:44:16 +0800

    table

---
+++ app/Models/PR_PublicationReport.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PR_PublicationReport extends Model
+{
+    use HasFactory;
+}
