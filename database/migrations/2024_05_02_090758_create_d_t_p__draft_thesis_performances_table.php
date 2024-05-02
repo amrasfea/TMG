94c706e28b50d599e8143715744525384bc936fb
ffakhira <fakhiraalysa03@gmail.com>
2024-05-02 17:10:26 +0800

    mm

---
+++ database/migrations/2024_05_02_090758_create_d_t_p__draft_thesis_performances_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('d_t_p__draft_thesis_performances', function (Blueprint $table) {
+            $table->string('DTP_DraftNum')->primary();
+            $table->string('DTP_Title');
+            $table->string('C_ID');
+            $table->foreign('C_ID')->references('C_ID')->on('c_r_m_p_s');
+            $table->string('P_platinumID');
+            $table->foreign('P_platinumID')->references('P_platinumID')->on('p__platinums');
+            $table->date('DTP_StartDate');
+            $table->date('DTP_CompletionDate');
+            $table->integer('DTP_PagesNum');
+            $table->string('M_mentorID');
+            $table->foreign('M_mentorID')->references('M_mentorID')->on('m_mentors');
+            $table->string('DTP_DDCgroup');
+            $table->integer('DTP_PrepareDays');
+            $table->integer('DTP_TotalPages');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('d_t_p__draft_thesis_performances');
+    }
+};
