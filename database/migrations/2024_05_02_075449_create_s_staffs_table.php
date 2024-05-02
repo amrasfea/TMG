6c01dbcef403db55e6edb02b8c5fc84b3f019650
ffakhira <fakhiraalysa03@gmail.com>
2024-05-02 15:55:46 +0800

    staff

---
+++ database/migrations/2024_05_02_075449_create_s_staffs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('s_staffs', function (Blueprint $table) {
+            $table->string('S_staffID')->primary();
+            $table->string('A_accountID');
+            $table->foreign('A_accountID')->references('A_accountID')->on('A_account');
+            $table->string('S_position');
+            $table->string('S_department');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('s_staffs');
+    }
+};
