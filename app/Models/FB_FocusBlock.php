126cbe4b787ebf0a9f1613ecbeb55b509b0dd497
ffakhira <fakhiraalysa03@gmail.com>
2024-05-02 12:39:49 +0800

    table

---
+++ app/Models/FB_FocusBlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FB_FocusBlock extends Model
+{
+    use HasFactory;
+}
