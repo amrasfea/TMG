845e2e72a39706d39ee09cccabf95dcb2113f648
Gin-chan1 <130429674+Gin-chan1@users.noreply.github.com>
2024-05-12 23:44:01 +0800

    co trolerererer

--- app/Http/Controllers/ManageDraftThesisPerformanceController.php
+++ app/Http/Controllers/ManageDraftThesisPerformanceController.php
@@ -6,5 +6,62 @@ use Illuminate\Http\Request;
 
 class ManageDraftThesisPerformanceController extends Controller
 {
-    //
+    public function GenerateDraftThesisPerformanceReport() {
+        return view('ManageDraftThesisPerformanceView.GenerateDraftThesisPerformanceReport');
+    }
+    public function DraftCompletionDateView() {
+        return view('ManageDraftThesisPerformanceView.Platinum.DraftCompletionDateView');
+    }
+    public function DraftDaystoPrepareView() {
+        return view('ManageDraftThesisPerformanceView.Platinum.DraftDaystoPrepareView');
+    }
+    public function DraftNumView() {
+        return view('ManageDraftThesisPerformanceView.Platinum.DraftNumView');
+    }
+    public function DraftStartDateView() {
+        return view('ManageDraftThesisPerformanceView.Platinum.DraftStartDateView');
+    }
+    public function DraftTotalPageView() {
+        return view('ManageDraftThesisPerformanceView.Platinum.DraftTotalPageView');
+    }
+    public function ThesisTitleView() {
+        return view('ManageDraftThesisPerformanceView.Platinum.ThesisTitleView');
+    }
+    public function AllPlatinumDraftCompletionDateView() {
+        return view('ManageDraftThesisPerformanceView.Mentor.AllPLatinumDraftCompletionDateView');
+    }
+    public function AllPlatinumDraftDaystoPrepareView() {
+        return view('ManageDraftThesisPerformanceView.Mentor.AllPlatinumDraftDaystoPrepareView');
+    }
+    public function AllPlatinumDraftNumView() {
+        return view('ManageDraftThesisPerformanceView.Mentor.AllPlatinumDraftNumView');
+    }
+    public function AllPlatinumDraftStartDateView() {
+        return view('ManageDraftThesisPerformanceView.Mentor.AllPlatinumDraftStartDateView');
+    }
+    public function AllPlatinumDraftTotalPageView() {
+        return view('ManageDraftThesisPerformanceView.Mentor.AllPlatinumDraftTotalPageView');
+    }
+    public function AllPlatinumThesisTitleView() {
+        return view('ManageDraftThesisPerformanceView.Mentor.AllPlatinumThesisTitleView');
+    }
+
+    public function PlatinumDraftCompletionDateView() {
+        return view('ManageDraftThesisPerformanceView.CRMP.PLatinumDraftCompletionDateView');
+    }
+    public function PlatinumDraftDaystoPrepareView() {
+        return view('ManageDraftThesisPerformanceView.CRMP.PlatinumDraftDaystoPrepareView');
+    }
+    public function PlatinumDraftNumView() {
+        return view('ManageDraftThesisPerformanceView.CRMP.PlatinumDraftNumView');
+    }
+    public function PlatinumDraftStartDateView() {
+        return view('ManageDraftThesisPerformanceView.CRMP.PlatinumDraftStartDateView');
+    }
+    public function PlatinumDraftTotalPageView() {
+        return view('ManageDraftThesisPerformanceView.CRMP.PlatinumDraftTotalPageView');
+    }
+    public function PlatinumThesisTitleView() {
+        return view('ManageDraftThesisPerformanceView.CRMP.PlatinumThesisTitleView');
+    }
 }
