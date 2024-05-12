15bd5293c756dcbd8f58897b8ae1d6f6a4e986ba
fikrishahril <165257512+fikrishahril@users.noreply.github.com>
2024-05-13 00:32:47 +0800

    edit

--- app/Http/Controllers/ExpertDomainController.php
+++ app/Http/Controllers/ExpertDomainController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ExpertDomainController extends Controller
 {
-    
+    public function AddExpertDomainInformation() {
+        return view('AddExpertDomainInformation.AddExpertDomainInformation');
+    }
+
+    public function AddResearchPublicationView() {
+        return view('AResearch&Publication.AddResearch&PublicationView');
+    }
 }
