aae09c3f62bcbb269252e5bd6c12ce052516ed59
amrasfea <sofeaamira51@gmail.com>
2024-05-12 17:58:54 +0800

    new navbar

--- resources/views/LoginView/MentorHomePage.blade.php
+++ resources/views/LoginView/MentorHomePage.blade.php
@@ -1,18 +1,56 @@
-@extends('MentorLayout')
+@extends('layout.MentorLayout')
 
 @section('MentorBar')
     <!-- Your navigation content goes here -->
-    <div style="margin-left:20%;padding:1px 20px;height:1000px; margin-top: 40px;">
-  <h2>Welcome to Thesis Pro System</h2>
-  <h3>Try to scroll this area, and see how the sidenav sticks to the page</h3>
-  <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
-  <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too long (for example if it has over 50 links inside of it).</p>
-  <p>Some text..</p>
-  <p>Some text..</p>
-  <p>Some text..</p>
-  <p>Some text..</p>
-  <p>Some text..</p>
-  <p>Some text..</p>
-  <p>Some text..</p>
-</div>
+   <!-- <div style="margin-left:20%;padding:1px 20px;height:1000px; margin-top: 40px;"> -->
+   <div class="main p-3">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <nav class="navbar bg-body-tertiary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                            <img src="/image/logo.png" alt="Logo" width="80" height="80" class="d-inline-block align-text-top">
+                        </a>
+                    </div>
+                </nav>
+                <a class="navbar-brand" href="#">Welcome To Thesis Master Gate !</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        
+                    </ul>
+                </div>
+            </nav>
+            <div class="row border ">
+                <div class="col-md-9">
+                    <div class="container border">
+                        <div class="card">
+                            <img src="{{ URL('images/Software testing.jpg') }}" alt="Software testing" width="100" height="100">
+                            <div class="card-body">
+                                <div class="container">
+                                    <h5 class="card-title">Card title</h5>
+                                </div>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+    <script>
+        const hamBurger = document.querySelector(".toggle-btn");
+
+        hamBurger.addEventListener("click", function () {
+            document.querySelector("#sidebar").classList.toggle("expand");
+        });
+
+        
+    </script>
 @endsection
