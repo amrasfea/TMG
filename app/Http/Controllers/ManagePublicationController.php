df158de7d15ad15e577c8356eef6cec89a5cddc1
ffakhira <fakhiraalysa03@gmail.com>
2024-05-16 05:24:20 +0800

    publication

--- app/Http/Controllers/ManagePublicationController.php
+++ app/Http/Controllers/ManagePublicationController.php
@@ -6,5 +6,50 @@ use Illuminate\Http\Request;
 
 class ManagePublicationController extends Controller
 {
-    //
+    public function AddPublication()
+    {
+        return view('ManagePublicationView.Platinum.AddPublication');
+    }
+
+    public function EditPublication()
+    {
+        return view('ManagePublicationView.Platinum.EditPublication');
+    }
+
+    public function OwnPublication()
+    {
+        return view('ManagePublicationView.Platinum.OwnPublication');
+    }
+
+    public function SearchPublication()
+    {
+        return view('ManagePublicationView.Platinum.SearchPublication');
+    }
+
+    public function ExpertDomainPublication()
+    {
+        return view('ManagePublicationView.Mentor.ExpertDomainPublication');
+    }
+
+    public function PlatinumPublication()
+    {
+        return view('ManagePublicationView.Mentor.PlatinumPublication');
+    }
+
+    public function PublicationReport()
+    {
+        return view('ManagePublicationView.Mentor.PublicationReport');
+    }
+
+    public function RegisteredPlatinum()
+    {
+        return view('ManagePublicationView.Mentor.RegisteredPlatinum');
+    }
+
+    public function SearchPlatinum()
+    {
+        return view('ManagePublicationView.Mentor.SearchPlatinum');
+    }
+
+
 }
