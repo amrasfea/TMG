e61c39bdfefac4f82bf960050e2625c34f3fbba1
fikrishahril <165257512+fikrishahril@users.noreply.github.com>
2024-05-18 09:04:54 +0800

    platinum

---
+++ resources/views/ExpertDomainView/Platinum/AddExpertDomainView.blade.php
@@ -0,0 +1,23 @@
+@extends('main.platinumMain')
+
+@section('Platinum')
+    <!-- Your navigation content goes here -->
+   <!-- <div style="margin-left:20%;padding:1px 20px;height:1000px; margin-top: 40px;"> -->
+   
+            
+
+
+
+    <div class="container mt-5">
+        <h1>Welcome to the Platinum Homepage</h1>
+        <p>This is the content of the Platinum homepage.</p>
+        <!-- Add more content as needed -->
+    </div>
+
+
+       
+    
+
+        
+    </script>
+@endsection
